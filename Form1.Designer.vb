ab5617860c89f69c7556299408147cb96867685a
JOSE DANIEL DIAZ <dandia28@hotmail.com>
2020-04-30 22:58:41 -0300

    primer subida

---
+++ Form1.Designer.vb
@@ -0,0 +1,88 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form reemplaza a Dispose para limpiar la lista de componentes.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Requerido por el Diseñador de Windows Forms
+    Private components As System.ComponentModel.IContainer
+
+    'NOTA: el Diseñador de Windows Forms necesita el siguiente procedimiento
+    'Se puede modificar usando el Diseñador de Windows Forms.  
+    'No lo modifique con el editor de código.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.txtNombre = New System.Windows.Forms.TextBox()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.btnRegistrar = New System.Windows.Forms.Button()
+        Me.grdAlumnos = New System.Windows.Forms.DataGridView()
+        CType(Me.grdAlumnos, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'txtNombre
+        '
+        Me.txtNombre.Font = New System.Drawing.Font("Microsoft Sans Serif", 15.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.txtNombre.Location = New System.Drawing.Point(63, 76)
+        Me.txtNombre.Name = "txtNombre"
+        Me.txtNombre.Size = New System.Drawing.Size(334, 31)
+        Me.txtNombre.TabIndex = 0
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Font = New System.Drawing.Font("Microsoft Sans Serif", 15.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label1.Location = New System.Drawing.Point(58, 38)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(196, 25)
+        Me.Label1.TabIndex = 1
+        Me.Label1.Text = "Ingrese su nombre:"
+        '
+        'btnRegistrar
+        '
+        Me.btnRegistrar.Location = New System.Drawing.Point(415, 69)
+        Me.btnRegistrar.Name = "btnRegistrar"
+        Me.btnRegistrar.Size = New System.Drawing.Size(160, 50)
+        Me.btnRegistrar.TabIndex = 2
+        Me.btnRegistrar.Text = "REGISTRAR ALUMNO"
+        Me.btnRegistrar.UseVisualStyleBackColor = True
+        '
+        'grdAlumnos
+        '
+        Me.grdAlumnos.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize
+        Me.grdAlumnos.Location = New System.Drawing.Point(63, 136)
+        Me.grdAlumnos.Name = "grdAlumnos"
+        Me.grdAlumnos.Size = New System.Drawing.Size(512, 332)
+        Me.grdAlumnos.TabIndex = 3
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(672, 480)
+        Me.Controls.Add(Me.grdAlumnos)
+        Me.Controls.Add(Me.btnRegistrar)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.txtNombre)
+        Me.Name = "Form1"
+        Me.Text = "Bienvenido al Registro"
+        CType(Me.grdAlumnos, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents txtNombre As TextBox
+    Friend WithEvents Label1 As Label
+    Friend WithEvents btnRegistrar As Button
+    Friend WithEvents grdAlumnos As DataGridView
+End Class
---
+++ Form1.vb
@@ -0,0 +1,43 @@
+﻿Imports MySql.Data.MySqlClient
+Public Class Form1
+    Private Sub btnRegistrar_Click(sender As Object, e As EventArgs) Handles btnRegistrar.Click
+
+        Dim conexion As MySqlConnection
+        conexion = New MySqlConnection
+
+        Dim cmd As New MySqlCommand
+        'variable de la clase 30-4
+        Dim ds As DataSet = New DataSet
+        Dim adaptador As MySqlDataAdapter = New MySqlDataAdapter
+
+        conexion.ConnectionString = "server=localhost; database=curso_visual;Uid=root;Pwd=;"
+
+        Try
+            conexion.Open()
+            'MsgBox("nos conectamos")
+            cmd.Connection = conexion
+
+            cmd.CommandText = "INSERT INTO alumnos(nombre) VALUES(@nombre)"
+            cmd.Prepare()
+
+            cmd.Parameters.AddWithValue("@nombre", txtNombre.Text)
+            cmd.ExecuteNonQuery()
+            txtNombre.Clear()
+
+            cmd.CommandText = "SELECT * FROM alumnos ORDER BY nombre ASC"
+            adaptador.SelectCommand = cmd
+            adaptador.Fill(ds, "Tabla")
+            grdAlumnos.DataSource = ds
+            grdAlumnos.DataMember = "Tabla"
+
+            conexion.Close()
+
+            ' MsgBox("Felcitaciones Usted fue ingresaro como alunno al curso de Daniel! ")
+
+
+        Catch ex As Exception
+            MsgBox(ex.Message)
+        End Try
+
+    End Sub
+End Class
